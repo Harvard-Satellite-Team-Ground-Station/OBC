9a42bea91ed8264c3c6fc5fd4a58c641b7463b19
Madison Davis <madisond2302@gmail.com>
2025-04-11 17:35:51 -0400

    config, battery.py now for can buses

--- OBC/subsystems/config.py
+++ OBC/subsystems/config.py
@@ -3,20 +3,43 @@
 
 
 # ++++++++++++++ Imports/Installs ++++++++++++++ #
-import Adafruit_BBIO.GPIO as GPIO
 
 
 
-# ++++++++++++++ Global Variables ++++++++++++++ #
-PORT_IMU_INPUT                  = GPIO.setup("P8_7", GPIO.IN)
-PORT_IMU_OUTPUT                 = GPIO.setup("P8_8", GPIO.OUT)
-PORT_MAGNETORQUER_INPUT         = GPIO.setup("P8_11", GPIO.IN)
-PORT_MAGNETORQUER_OUTPUT        = GPIO.setup("P8_12", GPIO.OUT)
-PORT_BATTERY_INPUT              = GPIO.setup("P8_16", GPIO.IN)
-PORT_NITHINOL_INPUT             = GPIO.setup("P8_17", GPIO.IN)
-PORT_NITHINOL_OUTPUT            = GPIO.setup("P8_18", GPIO.OUT)
-PORT_CAMERA_INPUT               = GPIO.setup("P9_23", GPIO.IN)
-PORT_CAMERA_OUTPUT              = GPIO.setup("P9_24", GPIO.OUT)
-PORT_ANTENNA_INPUT              = GPIO.setup("P9_25", GPIO.IN)
-PORT_ANTENNA_OUTPUT             = GPIO.setup("P9_26", GPIO.OUT)
+# ++++++++++++++ Can-Bus Variables ++++++++++++++ #
+"""
+Each can-bus is a collection of insertion pins where you can put wires
+Each can-bus will therefore be able to handle multiple sub-systems
+CAN_BUS_[subsystem]
+"""
+CAN_BUS_IMU                     = 'can1'
+CAN_BUS_MAGNETORQUER            = 'can1'
+CAN_BUS_NITHINOL                = 'can2'
+CAN_BUS_CAMERA                  = 'can2'
+CAN_BUS_BATTERY                 = ''        # it has its own input can bus
+CAN_BUS_ANTENNA                 = ''        # it has its own input can bus
+
+""" 
+PORT_IMU_INPUT                  = #GPIO.setup("P8_7", GPIO.IN)
+PORT_IMU_OUTPUT                 = #GPIO.setup("P8_8", GPIO.OUT)
+PORT_MAGNETORQUER_INPUT         = #GPIO.setup("P8_11", GPIO.IN)
+PORT_MAGNETORQUER_OUTPUT        = #GPIO.setup("P8_12", GPIO.OUT)
+PORT_BATTERY_INPUT              = #GPIO.setup("P8_16", GPIO.IN)
+PORT_NITHINOL_INPUT             = #GPIO.setup("P8_17", GPIO.IN)
+PORT_NITHINOL_OUTPUT            = #GPIO.setup("P8_18", GPIO.OUT)
+PORT_CAMERA_INPUT               = #GPIO.setup("P9_23", GPIO.IN)
+PORT_CAMERA_OUTPUT              = #GPIO.setup("P9_24", GPIO.OUT)
+PORT_ANTENNA_INPUT              = #GPIO.setup("P9_25", GPIO.IN)
+PORT_ANTENNA_OUTPUT             = #GPIO.setup("P9_26", GPIO.OUT)
+"""
+
+
+# ++++++++++++++ Can-ID Variables ++++++++++++++ #
+"""
+To know which wires should correspond to what, we'll use can-ids
+These correspond to the literal pin inputs
+CAN_ID_[subsystem]_[input/output]_[description]
+"""
+CAN_ID_BATTERY_INPUT_STATUS           = 0x100       # on can_bus 0, for pin 0x100, this'll be the wire for getting status
+CAN_ID_BATTERY_INPUT_TEMP             = 0x101       # on can_bus 0, for pin 0x101, this'll be the wire for getting temp
 
--- OBC/subsystems/subs_battery.py
+++ OBC/subsystems/subs_battery.py
@@ -7,11 +7,10 @@ import can
 from subsystems import config
 
 
-
 # ++++++++++++++ Class Definition ++++++++++++++ # 
 class SubsystemBattery:
     def __init__(self):
-        self.port_input = can.interface.Bus(channel=config.PORT_BATTERY_INPUT, bustype='socketcan')
+        self.bus = can.interface.Bus(channel=config.PORT_BATTERY_INPUT, bustype='socketcan')
 
     def data_func_battery_percentage(self):
         """
@@ -19,20 +18,30 @@ class SubsystemBattery:
         Conversion-chart for voltage to battery percentage
         """
         # Read ADC value (0-1.8V)
-        # Since battery is (0-4.2V), scale by 2.34
-        # Reverse voltage divider scaling (assuming 22kΩ / 47kΩ divider)
-        voltage = val * 2.34
-        voltage = voltage * (69 / 22) 
-    
-        if voltage >= 4.2:
-            return 100
-        elif voltage >= 3.95:
-            return 75
-        elif voltage >= 3.7:
-            return 50
-        elif voltage >= 3.5:
-            return 25
-        elif voltage >= 3.2:
-            return 0
-        else:
+        try:
+            msg = self.bus.recv(timeout=1.0)
+            if msg and msg.arbitration_id == config.CAN_ID_BATTERY_STATUS:
+                value = msg.data[0]  
+                # Since battery is (0-4.2V), scale by 2.34
+                voltage = value * 2.34
+                # Reverse voltage divider scaling (assuming 22kΩ / 47kΩ divider)
+                voltage = voltage * (69 / 22) 
+                if voltage >= 4.2:
+                    return 100
+                elif voltage >= 3.95:
+                    return 75
+                elif voltage >= 3.7:
+                    return 50
+                elif voltage >= 3.5:
+                    return 25
+                elif voltage >= 3.2:
+                    return 0
+                else:
+                    return -1
             return -1
+        except can.CanError as e:
+            print(f"[Battery] CAN error: {e}")
+            return -1
+    
+        
+
